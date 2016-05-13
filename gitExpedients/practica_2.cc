4a7983258b224f8a3fc7bd5d4dd128fe71f825e5
sergislabgg <sergislabgg@kubuntuX555LAB>
2016-05-13 15:30:53 +0200

    mas o menos coje forma... llibrerias ok menu ok .h ok .txt ok makefile ok
    100 lineas codigo mas o menos!

---
+++ gitExpedients/practica_2.cc
@@ -0,0 +1,123 @@
+//Programa principal: Almenys la funció main i les funcions addicionals que calgui o tingui sentit.
+
+
+#include <stdio.h>
+#include "practica_2.h"
+
+typedef struct sExpedients{
+    char nom;
+    char primerCognom;
+    char segonCognom;
+    int NIA;
+}sExpedients;
+
+typedef struct sAsignatures{
+    int num;
+    char tipus;
+    int curs;
+    int trim;
+    int codiAsig;
+    int credits;
+    char nomAsig;
+}sAsigatures;
+
+
+int carregar_fitxers(FILE * expedients,FILE * asignatures){
+    
+        expedients=fopen("dades\expedient_12345.txt","r");   //Obrim el fitxer "estudiants.txt" dins el punter de tipos fitxer f.
+        asignatures=fopen("dades\asgs-get.txt","r");   //Obrim el fitxer "estudiants.txt" dins el punter de tipos fitxer f.
+    
+        if(expedients && asignatures==NULL){                    //Aquesa clausula if, es per comprovar si el fitxer "estudiants.txt"
+            printf("ERROR \n");         //te algun coningut o si el programa el carrega correctament.
+            printf("Revisa el fitxer estudiants.txt");
+
+            return (0);
+        }
+}
+
+void alta_expedient(int nia_eval, sExpedients *e){
+    printf("A continuació podra donar d'alta un nou expedient\n");
+    printf("Per tal de que tot funcioni correctament, nececitem un NIA valid\n(Recordar que per que sigui valid te que tenir 5 digits \ni no pot ser cap NIA duplicat ja que no podem donar d'alta 2 persones amb el mateix NIA,): \n");
+    scanf("%d",&nia_eval);
+
+    for(int i=0;i<sEMAX;i++){
+        if(nia_eval==e[i].NIA){
+            int nia_error=0;
+            printf("ERROR\n");
+            printf("El NIA introduit ja forma part del programa, siusplau, cambii de nia o torni al menu principal.");
+            scanf("%d",&nia_error);
+            switch(nia_error){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                default:
+                    break;
+                    
+            }
+        }
+    };
+    
+}
+void entrar_matricula(){
+    
+}
+void entrar_notes_manual(){
+    
+}
+void estat_expedient(){
+    
+}
+
+int main() {
+    int menu=0;               //Declarem la variable que el menu fara servir.
+    sExpedients e[sEMAX];       //Declarem el vector del tipos expedients...
+                /*MAX es una constant que ens deixa posar un maxim de 999 alumnes o expedients de alumne*/
+    int nia_eval;             //Declarem una variable per comparar si el nia existeix...
+    
+    FILE * expedients;       //Declarem la variable que contindra el fitxer expedients.
+    FILE * asignatures;       //Declarem la variable que contindra el fitxer expedients.
+    
+    do{
+        printf("***********Menu inici UPF, expedients***********\n");
+        printf("Les operacions mínimes que ha d’implementar el programa són:\n");
+        printf("1. Carregar fitxers: carrega fitxers existents\n");
+        printf("2. Alta d’expedient: Registra un alumne nou i crea un expedient amb les dades generals però sense assignatures\n");
+        printf("3. Entrar matricula: crea expedients nous i modifica fitxers expedients existents\n");
+        printf("4. Entrar notes manualment: entra totes les notes d’una assignatura (i actualitza fitxers)\n");
+        printf("5. (opcional) Estat dels expedients: per cada expedient en memòria mostra el resum de l’expedient \n i indica en quina situació estroba dins el pla docent que està matriculat (veure secció 1.2)\n");
+        printf("Seleccioni una de les opcions llistades:\n");
+
+        scanf("%d",&menu);
+        switch(menu) {
+            case 1:
+                /*1. Carregar fitxers: carrega fitxers existents*/
+                carregar_fitxers(expedients,asignatures);
+                break;
+
+            case 2:
+                /*2. Alta d’expedient: Registra un alumne nou i crea un expedient amb les dades generals però sense assignatures*/
+                alta_expedient(nia_eval,e);
+                break;
+
+            case 3:
+                /*3. Entrar matricula: crea expedients nous i modifica fitxers expedients existents*/
+                entrar_matricula();
+                break;
+
+            case 4:
+                /*4. Entrar notes manualment: entra totes les notes d’una assignatura (i actualitza fitxers)*/
+                entrar_notes_manual();
+                break;
+
+            case 5:
+                /*"5. (opcional) Estat dels expedients: per cada expedient en memòria mostra el resum de l’expedient \n i indica en quina situació estroba dins el pla docent que està matriculat (veure secció 1.2)*/
+                estat_expedient();
+                break;
+                
+            default:printf("Opcio incorrecte*");
+        }
+    }while(menu!=6);
+    fclose(expedients);
+    fclose(asignatures);
+}
